808c5fffb39ec916a9b8314913afc5a3c8695f94
Taylor Epskamp <taylorepskamp@gmail.com>
2020-05-13 09:12:38 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactApp1/precache-manifest.54543bb16c30b454d1f48972e54e102c.js"
+  "/reactApp1/precache-manifest.e95ae11dc154416e3469b3524304a6ab.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/js/xlsx.d314d42f.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{389:function(n,o){},477:function(n,o){},478:function(n,o){}}]);
-//# sourceMappingURL=xlsx.d314d42f.chunk.js.map
